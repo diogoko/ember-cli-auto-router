cba858c61962381644d43ce1f4c609a4a36e3faf
Diogo Kollross <diogoko@gmail.com>
2014-09-10 22:14:39 -0300

    Converted to addon format

--- index.js
+++ index.js
@@ -1,62 +1,13 @@
-/* global requirejs */
+'use strict';
 
-/*
-function createChildrenFunction(rootRoute) {
-  return function() {
-    var resourceMap = this;
-    registerRoutes(resourceMap, rootRoute);
-  };
-}
+var path = require('path');
 
-function isLeafRoute(route) {
-  return Ember.keys(route).length == 0;
-}
+module.exports = {
+  name: 'ember-cli-auto-router',
 
-function registerRoutes(routerMap, rootRoute) {
-  Ember.keys(rootRoute).forEach(function(childRouteName) {
-    var childRoute = rootRoute[childRouteName];
-    if (isLeafRoute(childRoute)) {
-      routerMap.route(childRouteName);
-      console.debug('registered route ' + childRouteName);
-    } else {
-      routerMap.resource(childRouteName, createChildrenFunction(childRoute));
-      console.debug('registered resource ' + childRouteName);
+  treeFor: function(name) {
+    if (name == 'vendor') {
+      return path.join('node_modules', 'ember-cli-auto-router', 'vendor');
     }
-  });
-}
-
-function autoMap(routerMap) {
-  // TODO: use App.modulePrefix instead of \w+
-  var routesRegExp = new RegExp('^\\w+/routes/');
-
-  var rootRoute = {};
-  Ember.keys(requirejs._eak_seen).filter(function(key) {
-    return routesRegExp.test(key);
-  }).forEach(function(moduleName) {
-    var routePath = moduleName.replace(routesRegExp, '');
-
-    // Ignore built-in routes
-    if (routePath == 'application' || routePath == 'index') {
-      return;
-    }
-
-    var currentRoute = rootRoute;
-    routePath.split('/').forEach(function(part) {
-      // Add part to current route if it's not there yet
-      if (!currentRoute[part]) {
-        currentRoute[part] = {};
-      }
-
-      currentRoute = currentRoute[part];
-    });
-  });
-
-  registerRoutes(routerMap, rootRoute);
-}
-
-export default autoMap;
-*/
-
-module.exports = function() {
-  return 'oi';
+  }
 };
