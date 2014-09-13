b07090120d4abb735d2e26414ef1b04d0e9975a4
Diogo Kollross <diogoko@gmail.com>
2014-09-13 09:36:14 -0300

    Extracted mocks to a module

--- test/test.js
+++ test/test.js
@@ -1,54 +1,12 @@
 var should = require('should');
 
+var mocks = require('./mocks');
+FakeRouterDSL = mocks['FakeRouterDSL'];
+Ember = mocks['Ember'];
+setupModules = mocks['setupModules'];
+
 var autoMap = require('../tmp/vendor/ember-cli-auto-router')['default'];
 
-function FakeRouterDSL(parent) {
-  this.parent = parent;
-
-  var self = this;
-
-  this.routes = {};
-  Object.defineProperty(this.routes, 'names', {
-    get: function() {
-      return Object.keys(self.routes).sort();
-    }
-  });
-
-  this.resources = {};
-  Object.defineProperty(this.resources, 'names', {
-    get: function() {
-      return Object.keys(self.resources).sort();
-    }
-  });
-}
-
-FakeRouterDSL.prototype.route = function(name) {
-  this.routes[name] = true;
-};
-
-FakeRouterDSL.prototype.resource = function(name, callback) {
-  var child = new FakeRouterDSL(this);
-  callback.call(child);
-
-  this.resources[name] = child;
-};
-
-FakeRouterDSL.prototype.hasRoute = function(path) {
-};
-
-function setupModules(names) {
-  var _eak_seen = {};
-  names.forEach(function(name) {
-    _eak_seen[name] = true;
-  });
-  requirejs = { _eak_seen: _eak_seen };
-}
-
-Ember = {
-  keys: function(o) {
-    return Object.keys(o);
-  }
-};
 
 beforeEach(function() {
   map = new FakeRouterDSL();
